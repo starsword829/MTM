dcd907e9393c00644a7319125d89b59a5f54c246
starsword829 <ehcho@andrew.cmu.edu>
2016-01-13 13:11:18 -0500

    C

--- MTM_MASTER/MTM_MASTER.ino
+++ MTM_MASTER/MTM_MASTER.ino
@@ -1,7 +1,9 @@
 #define NUM_MODULES 2
 #define DATA_SIZE   4
-#define C_BLEED     0
 #define BLEED_TIME  1000
+
+#define C_BLEED     0
+#define C_STOP      1
 
 byte modules[NUM_MODULES]={0,1};
 unsigned long previousMillis=0;
@@ -9,6 +11,7 @@ unsigned long previousMillis=0;
 int error();
 int send(byte addr, byte data[], int n);
 int init();
+int bleedModule(byte addr);
 int bleed();
 void startTimer();
 int maxTimer(unsigned long t);
@@ -30,7 +33,7 @@ int init() {
             return error();
     }
     for(int i=0; i<NUM_MODULES; i++) {
-        if(!bleed(modules[i]))
+        if(!bleedModule(modules[i]))
             return error();
     }
 }
@@ -49,12 +52,21 @@ int error() {
     return -1;
 }
 
-int bleed(byte addr) {
-    if(!send(addr, C_BLEED))
+int bleedModule(byte addr) {
+    if(!bleed(addr, 0))
+        return error();
+    if(!bleed(addr, 1))
+        return error();
+}
+
+int bleed(byte addr, byte pump) {
+    byte temp[2] = {pump, C_BLEED};
+    if(!send(addr, temp, 2))
         error();
+    temp[1] = C_STOP;
     startTimer();
     while(massChange()<10 && !maxTimer(BLEED_TIME));
-    if(!send(addr, STOP))
+    if(!send(addr, temp, 2))
         error();
     return 0;
 }
