57da9ea32336f440459e0f269e54a2063318d738
starsword829 <ehcho@andrew.cmu.edu>
2016-01-13 12:58:18 -0500

    sniffles

--- MTM_MASTER/MTM_MASTER.ino
+++ MTM_MASTER/MTM_MASTER.ino
@@ -1,12 +1,17 @@
 #define NUM_MODULES 2
 #define DATA_SIZE   4
+#define C_BLEED     0
+#define BLEED_TIME  1000
 
 byte modules[NUM_MODULES]={0,1};
+unsigned long previousMillis=0;
 
 int error();
 int send(byte addr, byte data[], int n);
 int init();
 int bleed();
+void startTimer();
+int maxTimer(unsigned long t);
 
 void setup() {
     delay(100);
@@ -44,7 +49,32 @@ int error() {
     return -1;
 }
 
-int bleed() {
-    if()
+int bleed(byte addr) {
+    if(!send(addr, C_BLEED))
+        error();
+    startTimer();
+    while(massChange()<10 && !maxTimer(BLEED_TIME));
+    if(!send(addr, STOP))
+        error();
     return 0;
+}
+
+void startTimer() {
+    previousMillis=millis();
+}
+
+int maxTimer(unsigned long t) {
+    unsigned long currentMillis=millis();
+    if((unsigned long)(currentMillis-previousMillis)>=t) {
+        return -1;
+    }
+    return 0;
+}
+
+int massFSR() {
+    return -1;
+}
+
+int massChange() {
+    return -1;
 }
